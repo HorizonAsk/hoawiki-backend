2b2c3c7fbc83edce27fae1121c6a5a8564d6f908
Han Y.B <yanbohan98@gmail.com>
2021-12-25 00:42:48 +0800

    Initial Version.

---
+++ src/test/java/top/horizonask/hoawiki/HoawikiApplicationTests.java
@@ -0,0 +1,13 @@
+package top.horizonask.hoawiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoawikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
